8ccf6d679796cd76838610585a7395978fcffac3
anapolg <ana.pol@quobis.com>
2018-02-21 15:30:22 +0100

    Removed initial code from SONATA. Basic unit test.

--- gui/src/scripts/components/control-menu/controlmenu.module.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import { ControlMenuComponent } from './controlmenu.component';
-
-export const ControlMenuModule = angular.module('controlmenu', [])
-    .component('svControlMenu', ControlMenuComponent)
-    .name;
-
-export default ControlMenuModule;
